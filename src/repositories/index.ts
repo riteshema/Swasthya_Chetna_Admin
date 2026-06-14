6ca2aa3c174f4e95e6fc60caf69938040abd12dd
Lodhi2005yash <lodhiyash663@gmail.com>
2026-06-14 13:28:04 +0530

    fix: add delete query function and update logic

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,4 +1,8 @@
+// export { default as AuthRepository } from "./auth_repository";
+// export { default as UserRepository } from "./user_repository";
+
+// export { QueryRepository } from "./query_repository";
+
 export { default as AuthRepository } from "./auth_repository";
 export { default as UserRepository } from "./user_repository";
-
 export { QueryRepository } from "./query_repository";
